1e267915d8d5235253876efa3c3135e0d8c351df
Jayprajapati19 <jayp37679@gmail.com>
2025-01-02 08:40:08 +0530

    customReact

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -1,14 +1,33 @@
-function customRender(reactElemen, container) {}
+function customRender(reactElement, container) {
+  /*
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  domElement.setAttribute("href", reactElement.props.href);
+
+  domElement.setAttribute("target", reactElement.props.target);
+
+  container.appendChild(domElement);
+  */
+
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+
+  for (const prop in reactElement.props) {
+    if (prop === "children") continue;
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+  container.appendChild(domElement);
+}
 
 const reactElement = {
   type: "a",
   props: {
-    herf: "http://reactjs.org",
+    href: "https://google.org",
     target: "_blank",
   },
-  Children: "Click me to visit google",
+  children: "Click me to visit google",
 };
 
 const mainContainer = document.querySelector("#root");
 
-customRender(reactElement.mainContainer);
+customRender(reactElement, mainContainer);
