8326272b40f58d20d5871708378d81ab88c690f7
Jayprajapati19 <jayp37679@gmail.com>
2025-01-04 23:27:22 +0530

    redux

--- 10reduxToolkitTodo/src/features/todo/todoSlice.js
+++ 10reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -11,9 +11,16 @@ export const todoSlice = createSlice({
     addTodo: (state, action) => {
       const todo = {
         id: nanoid(),
-        text: "Hellow world",
+        text: action.payload,
       };
+      state.todos.push(todo);
     },
-    removeTodo: () => {},
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
   },
 });
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
