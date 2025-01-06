8c4fa61c698c9c01e7bba8203a64f3b5c4d61d62
Jayprajapati19 <jayp37679@gmail.com>
2025-01-06 23:38:08 +0530

    input component

--- socialAppWrite/src/components/Input.jsx
+++ socialAppWrite/src/components/Input.jsx
@@ -8,9 +8,28 @@ const Input = React.forwardRef(function Input({
     ...props
 
 }, ref) {
-    return <h1>text</h1>
+
+    const id = useId()
+
+    return (
+        <div className='w-full  '>
+            {label && <label
+                className='inline-block mb-1 pl-1'
+                htmlFor={id}>
+                {label}
+            </label>
+            }
+            <input
+                type={type || 'text'}
+                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+                ref={ref}
+                {...props}
+                id={id}
+            />
+
+        </div>
+    )
 })
 
-5: 32
 
 export default Input
