6f930807f8c6249f81c4bdc9b7635db0dffca9ae
Jayprajapati19 <jayp37679@gmail.com>
2025-01-06 22:21:20 +0530

    components

---
+++ socialAppWrite/src/components/Input.jsx
@@ -0,0 +1,16 @@
+import React, { useId } from 'react'
+
+
+const Input = React.forwardRef(function Input({
+    label,
+    type = 'text',
+    className = '',
+    ...props
+
+}, ref) {
+    return <h1>text</h1>
+})
+
+5: 32
+
+export default Input
